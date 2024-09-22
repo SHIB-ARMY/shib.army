7ffb801c51d9fa92386afa1db2cdff4bf9488729
Mitsuki Ryo Shimamoto <156122964+MRShimamoto@users.noreply.github.com>
2024-09-22 12:00:43 +0000

    align twitter

--- docs/script.js
+++ docs/script.js
@@ -12,8 +12,8 @@ var init = function () {
   var isSmall = window.innerWidth < 1000;
 
   var ps = new ParticleSlider({
-    ptlGap: isMobile || isSmall ? 3 : 0,
-    ptlSize: isMobile || isSmall ? 3 : 1,
+    ptlGap: isMobile || isSmall ? 4 : 4,
+    ptlSize: isMobile || isSmall ? 4 : 3,
     width: 1e9,
     height: 1e9
   });
